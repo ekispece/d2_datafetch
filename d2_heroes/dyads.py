67d7c1ae9c9687900efe1a802cfe25b54d92b67d
ekispece <ekispece@gmail.com>
2016-08-02 22:51:10 -0300

    Improved dyads calculation algorithm

--- d2_heroes/dyads.py
+++ d2_heroes/dyads.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+from collections import Counter
+
 from d2_db import db
 
 
@@ -8,29 +10,46 @@ def calculate_dyads():
     database = db.get_database()
     heroes_collection = database.heroes
     matches_collection = database.match_details
+    dyads_collection = database.heroes_dyads
+    heroes_dyads = {}
+    heroes_dyads_list = []
 
-    hero_dyad = {}
+    total_matches = matches_collection.count()
+    matches = 0
 
     for hero in heroes_collection.find():
-        hero_dyad['hero_id'] = hero['id']
-        hero_dyad['dyads'] = {}
-        for match in matches_collection.find({'players.hero_id': hero['id']}):
-            winner = match['winner']
-            this_hero_team = [h for h in match['players'] if h['hero_id'] == hero['id']][0]['team']
+        heroes_dyads[hero['id']] = {
+            'hero_id': hero['id'],
+            'wins': Counter(),
+            'matches': Counter()
+        }
 
-            win = 0
-            if this_hero_team == winner:
-                win = 1
+    for match in matches_collection.find():
+        matches += 1
+        if matches % 1000 == 0:
+            print('Parsed ', matches, 'matches out of ', total_matches)
+        winner = match['winner']
+        players = match['players']
+        assert len(players) == 10
+        for player in players:
+            his_team = [x for x in players if not x == player and x['team'] == player['team']]
+            assert len(his_team) == 4
+            if player['team'] == winner:
+                heroes_dyads[player['hero_id']]['wins'].update([x['hero_id'] for x in his_team])
+            heroes_dyads[player['hero_id']]['matches'].update([x['hero_id'] for x in his_team])
 
-            other_heroes = [h for h in match['players'] if
-                            not h['hero_id'] == hero['id'] and h['team'] == this_hero_team]
-            assert len(other_heroes) == 4
-            for hero_id in [x['hero_id'] for x in other_heroes]:
-                if hero_dyad['dyads'].get(str(hero_id)) is None:
-                    hero_dyad['dyads'][str(hero_id)] = {'matches': 0, 'wins': 0}
-                hero_dyad['dyads'][str(hero_id)]['matches'] += 1
-                hero_dyad['dyads'][str(hero_id)]['wins'] += win
+    for hero in heroes_collection.find():
+        hd = {
+            'hero_id': hero['id'],
+            'dyads': {}
+        }
+        for key, val in heroes_dyads[hero['id']]['matches'].items():
+            hd['dyads'][str(key)] = {
+                'matches': val,
+                'wins': heroes_dyads[hero['id']]['wins'].get(key, 0)
+            }
+        heroes_dyads_list.append(hd)
 
-        database.heroes_dyads.insert(hero_dyad)
+    assert len(heroes_dyads_list) == 111
 
-        hero_dyad.clear()
+    dyads_collection.insert_many(heroes_dyads_list)
