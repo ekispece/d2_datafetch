989edca93475273c624f08b1c04aeea8a55a9896
ekispece <ekispece@gmail.com>
2016-08-02 23:42:16 -0300

    minor bug fix

--- d2_ml/load_input_data.py
+++ d2_ml/load_input_data.py
@@ -10,9 +10,9 @@ from d2_db import db
 def keywithmaxval(d):
     """ a) create a list of the dict's keys and values;
         b) return the key with the max value"""
-    v = list(d.values())
+    v = [j['bought'] for j in d.values()]
     k = list(d.keys())
-    return k[v.index(max(v['bought']))]
+    return k[v.index(max(v))]
 
 
 def prepare_data():
